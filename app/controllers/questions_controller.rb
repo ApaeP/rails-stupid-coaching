bca5771517baea757965a6b8eda828870444341d
Paul Portier <paulportier@gmail.com>
2019-11-11 12:15:33 +0100

    stupid coaching on rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,9 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answers = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!"]
+  end
+end
